ee6fcb8e21b0ad25f83944e0e87482b7a26cfb2b
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2017-03-15 17:42:15 -0700

    did the post submit button for tasks

--- public/js/todos.js
+++ public/js/todos.js
@@ -1,6 +1,8 @@
 //Check off specific To dos by clicking
 "use strict";
 
+var buttonAddTask = document.getElementById("buttonAddTask")
+
 $("#sectionNewList ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
@@ -12,14 +14,29 @@ $("ul").on("click", "span", function(event){
 	event.stopPropagation();
 });
 
+// $("input[type = 'text']").keypress(function(){
+// 	if(event.which === 13){
+// 		 let todoText = $(this).val();
+// 		 $(this).val("");
+// 		 $(".sectionLists ul").append("<li><span><i class= 'fa fa-trash'></i></span>" + todoText + "</li>");
+// 	}
+// });
+
 $("input[type = 'text']").keypress(function(){
 	if(event.which === 13){
-		 let todoText = $(this).val();
-		 $(this).val("");
-		 $(".sectionLists ul").append("<li><span><i class= 'fa fa-trash'></i></span>" + todoText + "</li>");
+	clickInputInsert(buttonAddTask);
+	// 	 let todoText = $(this).val();
+	// 	 $(this).val("");
+	// 	 $(".sectionLists ul").append("<li><span><i class= 'fa fa-trash'></i></span>" + todoText + "</li>");
 	}
 });
 
 $(".fa-plus").click(function(){
 	$("input[type = 'text']").fadeToggle();
 });
+
+
+//ACTIVATE INPUT TYPE=FILE TAGS BY CLICKING AT DIFFERENT BUTTONS
+function clickInputInsert(inputTag){
+  inputTag.click();
+}
